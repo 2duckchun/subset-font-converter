4b1c5ced0eae56193964403e797aed0fd3141a19
2duckchun <kkts9308@gmail.com>
2025-07-25 16:14:15 +0900

    init - create subset-font-converter

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import fs from "node:fs/promises";
+import subsetFont from "subset-font";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const src = await fs.readFile(resolve(__dirname, "./FreesentationVF.ttf"));
+const chars = await fs.readFile(resolve(__dirname, "./glyphs.txt"), "utf8");
+
+const subset = await subsetFont(src, chars, {
+  targetFormat: "woff2",
+  variationAxes: {
+    wght: { min: 100, max: 900 },
+  },
+});
+
+await fs.writeFile(resolve(__dirname, "./FreetenSubset.woff2"), subset);
+
+console.log("서브셋 세팅 완료! 용량:", (subset.length / 1024).toFixed(1), "KB");
